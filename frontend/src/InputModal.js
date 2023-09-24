a3f1dd6929e4b111843efff8e05e60c877bac6f0
Animmah <mahaboleaniket575@gmail.com>
2023-09-24 12:45:24 +0530

    modal changes

--- frontend/src/InputModal.js
+++ frontend/src/InputModal.js
@@ -49,22 +49,22 @@ const InputModal=({isOpen,onRequestClose, weekDay})=>{
                 <h2>Set Schedule</h2>
                 <form className="form-container" onSubmit={handleSubmit}>
 
-                    <div style={label_style}>Start time</div>
-                    <input name="startTime" type="text" value={startTime} onChange={handleChange}/>
+                    {/* <div style={label_style}>Start time</div> */}
+                    <input name="startTime" type="text" value={startTime} onChange={handleChange} placeholder="Start Time"/>
                         <select name="startAM" onChange={handleChange} value={startAM}>
                             <option value="AM">AM</option>
                             <option value="PM">PM</option>
                         </select>
 
-                    <div style={label_style}>End time</div>
-                    <input name="endTime" type="text" value={endTime} onChange={handleChange}/>
+                    {/* <div style={label_style}>End time</div> */}
+                    <input name="endTime" type="text" value={endTime} onChange={handleChange} placeholder="End Time"/>
                         <select name="endAM" onChange={handleChange} value={endAM}>
                             <option value="AM">AM</option>
                             <option value="PM">PM</option>
                         </select>
 
-                    <div style={label_style}>Subject</div>
-                    <input name="subject" type="text" value={subject} onChange={handleChange}/>
+                    {/* <div style={label_style}>Subject</div> */}
+                    <input name="subject" type="text" value={subject} onChange={handleChange} placeholder="Subject"/>
                     
                     <button  className='save-btn' type="submit">Save</button>
                 </form>
